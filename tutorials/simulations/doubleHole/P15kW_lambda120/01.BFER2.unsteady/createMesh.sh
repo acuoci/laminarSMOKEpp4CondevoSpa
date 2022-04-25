a3967012ef946472618133ae710bfacb4b5d4260
Alberto Cuoci <alberto.cuoci@polimi.it>
2022-04-25 20:09:59 +0200

    Added tutorial: doubleHole

---
+++ tutorials/simulations/doubleHole/P15kW_lambda120/01.BFER2.unsteady/createMesh.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+cd "${0%/*}" || exit                                # Run from this directory
+. ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions        # Tutorial run functions
+. ${WM_PROJECT_DIR:?}/bin/tools/CleanFunctions      # Tutorial clean functions
+#------------------------------------------------------------------------------
+
+# Remove previous meshes
+rm -r constant/polyMesh
+rm -r constant/fluid/polyMesh
+rm -r constant/solid/polyMesh
+
+# Mesh conversion
+fluentMeshToFoam  ../../Meshes/Mesh_80k.msh  -writeSets -writeZones
+setsToZones -noFlipMap
+
+# Renumbering
+renumberMesh -overwrite
+
+# Scaling (if needed)
+# transformPoints -scale "(1e-3 1e-3 1e-3)" -overwrite
+
+# Split regions
+splitMeshRegions -cellZonesOnly -overwrite 
+
+# Remove unsplitted mesh
+rm -r constant/polyMesh
+
+# Check mesh
+checkMesh -region fluid
+checkMesh -region solid
+
+# Manual operations
+# Remember to change the boundary names and types
+
